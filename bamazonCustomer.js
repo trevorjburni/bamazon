9a331115ceab4db588596a6c47242da8d08cb92f
Pilot <trevorjburni@gmail.com>
2020-01-18 15:57:35 -0700

    Inquirer is properly prompting the user for input on start up

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,7 +3,9 @@ var inquirer = require("inquirer");
 // import mysql
 var mysql = require("mysql");
 // import console-table-printer
-var { printTable } = require("console-table-printer");
+var {
+    printTable
+} = require("console-table-printer");
 // create the connection
 var connection = mysql.createConnection({
     host: "localhost",
@@ -16,18 +18,45 @@ var connection = mysql.createConnection({
     database: "bamazon"
 });
 // establish connection and log out connection id
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     readProducts();
 });
 
+// function which prompts the user what product they would like to buy
+function purchase() {
+    inquirer
+        .prompt([{
+                name: "item",
+                type: "input",
+                message: "What's the ID of the product you would like to buy?",
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many would you like to buy?",
+                validate: function (value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                    }
+                    return false;
+                }
+            }
+        ]).then(function (answer) {
+            console.log(answer.item, answer.quantity);
+        });
+}
+
+// function to display all products and data associated with each product
 function readProducts() {
     console.log("All products we currently have... \n");
-    connection.query("SELECT * FROM products", function(err, res) {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
         // Log all results of the select statement
         printTable(res);
+        console.log("\n");
+        purchase();
         connection.end();
     });
-}
+};
